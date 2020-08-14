8437cde9fa647caae320d3d7cd99e99542be3b8f
Brian Donnoe <dunow33@hotmail.com>
2020-08-14 03:50:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     sentences_array = self.split(/[.!?]/)
-    sentences_array.reject { |c| c.empty? }
+    sentences_array.delete("")
     count = sentences_array.size
     #puts sentences_array
     count
